dba374eca266a808e4d11bdf0c188c85da82646b
kumkum_20 <kumkumss042@gmail.com>
2025-12-21 21:58:19 +0530

    Reverse element of an arraylist in java

--- Array/ReversingArray.java
+++ Array/ReversingArray.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class ReversingArray {
     public static void reverseArray(ArrayList<Integer> arr){
         int i=0;
-        int j=arr.size()-1;
+        int j=arr.size()-1; //to reverse upto any index just update value of i
         while(i<j){
             int temp=arr.get(i);
             arr.set(i,arr.get(j));
