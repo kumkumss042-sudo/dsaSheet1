19d298a6125eb31674eef0a19899f4c8b48c790f
kumkum_20 <kumkumss042@gmail.com>
2025-12-21 21:42:12 +0530

    Reversed the elements of an array in Java

--- Array/ReversingArray.java
+++ Array/ReversingArray.java
@@ -1,31 +1,30 @@
 package Array;
 import java.util.*;
 public class ReversingArray {
-    public static void swap(int arr[],int i,int j){
-        int temp=arr[i];  //for swapping
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
-    public static void reverseArray(int arr[]){
-        int i=0,j=arr.length-1;  //two pointer approach
+    public static void reverseArray(ArrayList<Integer> arr){
+        int i=0;
+        int j=arr.size()-1;
         while(i<j){
-            swap(arr,i,j);  //time complexity-->O(n)
-            i++;            //space complexity-->O(1)
+            int temp=arr.get(i);
+            arr.set(i,arr.get(j));
+            arr.set(j,temp);
+            i++;
             j--;
         }
-    }
+}
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter size of array");
         int n=sc.nextInt();
         System.out.println("enter elements");
-        int arr[]=new int[n];
+        ArrayList<Integer>arr=new ArrayList<>();
         for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+            arr.add(sc.nextInt());
         }
         reverseArray(arr);
         for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
+            System.out.print(arr.get(i)+" ");
         }
     }
 }
